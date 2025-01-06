6380cadfd9908f715eb963c6d3df8db4780b7a67
KodakWac <mylifeis11111@gmail.com>
2025-01-06 13:29:22 -0800

    : char count added

--- main.py
+++ main.py
@@ -2,10 +2,22 @@ def wordCount(string):
         words = string.split()
         return len(words)
 
+def countChars(file_contents):
+     char_count = {}
+     for char in file_contents:
+          charLower = char.lower()
+          if charLower in char_count:
+               char_count[charLower] += 1
+          else:
+              char_count[charLower] = 1 
+
+     return char_count
+
 def main():
     with open('books/frankenstein.txt') as f:
         file_contents = f.read()
     print(wordCount(file_contents))
+    print(countChars(file_contents))
 
 
 if __name__ == '__main__':
