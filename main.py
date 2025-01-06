dd36b4bf49a0d103415c925f846abe9bcf90c282
KodakWac <mylifeis11111@gmail.com>
2025-01-06 12:57:32 -0800

    added word count

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+def wordCount(string):
+        words = string.split()
+        return len(words)
+
 def main():
     with open('books/frankenstein.txt') as f:
         file_contents = f.read()
-    print(file_contents)
+    print(wordCount(file_contents))
+
 
 if __name__ == '__main__':
     main()
