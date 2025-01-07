55767aa24234f39d084dbe9a2ba84c4bfeb0fab6
KodakWac <mylifeis11111@gmail.com>
2025-01-06 21:06:56 -0800

    added sorted dictionary

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ def wordCount(string):
         words = string.split()
         return len(words)
 
-def countChars(file_contents):
-     char_count = {}
-     for char in file_contents:
-          charLower = char.lower()
-          if charLower in char_count:
-               char_count[charLower] += 1
-          else:
-              char_count[charLower] = 1 
 
-     return char_count
+def countChars(file_contents):
+    char_count = {}
+    for char in file_contents:
+        charLower = char.lower()
+        if charLower in char_count:
+            char_count[charLower] += 1
+        else:
+            char_count[charLower] = 1
+
+    sorted_char_count = sorted(char_count.items(), key=lambda item: item[1], reverse=True)
+    return sorted_char_count
 
 def main():
     with open('books/frankenstein.txt') as f:
